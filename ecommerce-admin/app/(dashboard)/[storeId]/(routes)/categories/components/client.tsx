4f4bab9ecc43da8ae8ab14271a1edf9f4ae87e4c
Trần Ngọc Đăng Khôi <khoitranngocdg@gmail.com>
2023-07-08 21:21:34 +0700

    Categories first update
    update the elements for categories.

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { ApiList } from "@/components/ui/api-list"
+import { Button } from "@/components/ui/button"
+import { DataTable } from "@/components/ui/data-table"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import { CategoryColumn, columns } from "./columns"
+
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
+
+interface CategoryClientProps {
+  data: CategoryColumn[]
+}
+
+export const CategoryClient: React.FC<CategoryClientProps> = ({
+  data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+    
+    return (
+        <>
+            <div className="flex items-center justify-between">
+              <Heading 
+                title={`Categories (${data.length})`}
+                description="Manage categories for your store"
+              />
+              <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
+                <Plus className="mr-2 h-4 w-4"/>
+                Add New
+              </Button>
+            </div>
+            <Separator />
+            <DataTable searchKey="name" columns={columns} data={data} />
+            <Heading title="API" description="API calls for Categories" />
+            <Separator />
+            <ApiList entityName="categories" entityIdName="categoryId"/>
+        </>
+    )
+}
